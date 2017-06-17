f309eccbd46198c7d46d5e60e8049836c7c703d7
Ziv Scully <zivscully@gmail.com>
2017-06-17 17:27:38 -0400

    Make Picker return vector of weights instaed of single element.

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,6 @@ module Lib
     ) where
 
 import Picker
-import Data.List.NonEmpty
 
 someFunc :: IO ()
-someFunc = print (pick (maxBy id) (1 :| [2, 3, 5, 67, 2, 53, 356]))
+someFunc = print (pick (maxBy id) [1, 2, 3, 2, 3, 0])
--- src/Picker.hs
+++ src/Picker.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Picker
@@ -10,29 +10,46 @@ module Picker
   , pick
   , maxBy
   , minBy
+  , maxWeightedBy
+  , minWeightedBy
   ) where
 
-import Data.Ord (Down(..))
-import Data.Semigroup (Semigroup(..))
-import Data.Semigroup.Foldable (Foldable1(..))
+import Control.Lens ( (&), (%~), _1, mapped )
+import Control.Monad.Writer ( Writer, runWriter, tell )
+import Data.Function ( on )
+import Data.Semigroup ( Semigroup, Max(..), Min(..), Option(..) )
 
-data MaxKey k v = MaxKey k v deriving Show
+-- A function that assigns a weight to each element in a collection.
+type Picker a =
+  forall f i. Traversable f =>
+  (i -> a) -> f i -> f (i, Double)
 
-instance Ord k => Semigroup (MaxKey k v) where
-  kv1@(MaxKey k1 _) <> kv2@(MaxKey k2 _) =
-    case compare k1 k2 of
-      LT -> kv2
-      _ -> kv1
-
-type Picker a = forall f i. Foldable1 f => (i -> a) -> f i -> i
-
-pick :: Foldable1 f => Picker a -> f a -> a
+pick :: Traversable f => Picker a -> f a -> f (a, Double)
 pick p xs = p id xs
 
-maxBy :: Ord b => (a -> b) -> Picker a
-maxBy metric item xs = i
+-- The idea is that `dir` is either `Min` or `Max`.
+optWeightedBy :: (Eq c, Semigroup c) => (b -> c) -> (a -> (b, Double)) -> Picker a
+optWeightedBy dir metricWeight item xs = ys
   where
-    MaxKey _ i = foldMap1 (\i -> (MaxKey (metric (item i)) i)) xs
+    (ys, metricMax) = runWriter (traverse go xs)
+    mw = metricWeight . item & mapped . _1 %~ Option . Just . dir
+    go i = do
+      let (metricMe, weightMe) = mw i
+          weight = if metricMe == metricMax then weightMe else 0
+      tell metricMe
+      return (i, weight)
+
+maxWeightedBy :: Ord b => (a -> (b, Double)) -> Picker a
+maxWeightedBy = optWeightedBy Max
+
+minWeightedBy :: Ord b => (a -> (b, Double)) -> Picker a
+minWeightedBy = optWeightedBy Min
+
+optBy :: (Eq c, Semigroup c) => (b -> c) -> (a -> b) -> Picker a
+optBy con metric = optWeightedBy con ((,1) . metric)
+
+maxBy :: Ord b => (a -> b) -> Picker a
+maxBy = optBy Max
 
 minBy :: Ord b => (a -> b) -> Picker a
-minBy metric = maxBy (Down . metric)
+minBy = optBy Min
