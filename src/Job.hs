2cdce555a5c07de60c40425cfcf2ee9a3c09ac89
Ziv Scully <zivscully@gmail.com>
2017-06-23 00:40:48 -0400

    Refactor JobBase out of JobOptimal.

--- src/Job.hs
+++ src/Job.hs
@@ -11,9 +11,10 @@ module Job
   , randomJob
   ) where
 
-import Control.Monad ( replicateM )
 import Control.Monad.State ( runState, state )
 import Data.Foldable ( foldl' )
+import Data.List.NonEmpty ( NonEmpty(..) )
+import qualified Data.List.NonEmpty as Ne
 import Data.Semigroup.Foldable ( Foldable1 )
 import System.Random
 
@@ -28,14 +29,15 @@ newtype Grade = Grade Double
 
 class IsJob job where
   -- Create a job given number of tasks, starting age, and completion ages.
-  -- Can assume the number of tasks is the number of completion ages.
-  create :: Int -> Time -> (Time, [Time]) -> job
+  -- Should use one of `jbPrepareFirst` or `jbPrepareParallel`.
+  fromJb :: JobBase -> job
   -- Measures time relative to some arbitrary base point, not actual age.
   -- The base point can change with each transition.
   ageOf :: job -> Time
   gradeOf :: job -> Grade
   withGrade :: job -> Grade -> job
   -- Gives state of the job right before and right after its next transition.
+  -- Should use one of `jbTransitionFirst` or `jbTransitionParallel`.
   nextTransition :: job -> (job, Maybe job)
   ageAtGrade :: job -> Grade -> Time
   ageAtGrade = ageAtGradeDefault
@@ -72,9 +74,8 @@ randomJob gen =
   flip runState gen $ do
     numTasks <- genNumTasks
     ageStart <- genAgeStart
-    ageFirstDone <- genPareto ageStart
-    agesRestDone <- replicateM (numTasks - 1) (genPareto ageStart)
-    return $ create numTasks ageStart (ageFirstDone, agesRestDone)
+    agesDone <- traverse genPareto (ageStart :| replicate (numTasks - 1) ageStart)
+    return (fromJb Jb{..})
   where
     genNumTasks = state (randomR (1, 7))
     genAgeStart = return (Time 1.0)
@@ -85,23 +86,30 @@ randomJob gen =
 -- All jobs have no precedence constraints.
 -- Within each job, all tasks are shifted Pareto of shape 2 and same scale.
 
+data JobBase = Jb{
+    ageStart :: Time
+  , agesDone :: NonEmpty Time
+  } deriving Show
+
 -- Optimal policy: grade is fair reward, tasks served one at a time.
 data JobOptimal = Jo{
     -- Must be at least `ageRest`.
     ageFirst :: Time
     -- Should be at least `ageFirst` while job is in system.
     -- But using `withGrade` in hypothetical computations are fine.
-  , ageFirstDone :: Time
   , ageRest :: Time
-    -- All must be at least `ageRest`.
-  , agesRestDone :: [Time]
-    -- Must be length of `agesRestDone`.
+    -- Must be `length (agesDone jbJo) - 1`.
   , numRest :: Int
+  , jbJo :: JobBase
   } deriving Show
 
 instance IsJob JobOptimal where
-  create numTasks ageStart (ageFirstDone, agesRestDone) =
-    Jo{ ageFirst = ageStart, ageRest = ageStart, numRest = numTasks - 1, .. }
+  fromJb jb@Jb{..} = Jo{
+      ageFirst = ageStart
+    , ageRest = ageStart
+    , numRest = length agesDone - 1
+    , jbJo = jbPrepareFirst jb
+    }
 
   ageOf Jo{..} = ageFirst + (fromIntegral numRest * ageRest)
 
@@ -117,17 +125,29 @@ instance IsJob JobOptimal where
 
   nextTransition j@Jo{..} = (jPre, jqPost)
     where
-      jPre = j{ ageFirst = ageFirstDone }
-      jqPost =
-        case agesRestDone of
-          [] -> Nothing
-          (age : ages) ->
-            Just jPre{
-                ageFirst = ageRest
-              , ageRest = age
-              , agesRestDone = ages
-              , numRest = numRest - 1
-              }
+      jPre = j{ ageFirst = Ne.head agesDone }
+      jqPost = do
+        jbPost <- jbTransitionFirst jbJo
+        return jPre{
+            ageFirst = ageRest
+          , numRest = numRest - 1
+          , jbJo = jbPost
+          }
+      Jb{..} = jbJo
+
+jbPrepareFirst :: JobBase -> JobBase
+jbPrepareFirst = id
+
+jbPrepareParallel :: JobBase -> JobBase
+jbPrepareParallel jb@Jb{..} = jb{ agesDone = Ne.sort agesDone }
+
+jbTransitionFirst jb@Jb{..} = do
+  ages <- snd (Ne.uncons agesDone)
+  return jb{ agesDone = ages }
+
+jbTransitionParallel jb@Jb{..} = do
+  ages <- snd (Ne.uncons agesDone)
+  return jb{ agesDone = ages }
 
 repeated :: Int -> (a -> a) -> a -> a
 repeated n f x = foldl' (flip ($)) x (replicate n f)
